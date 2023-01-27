40a52f8c40e6e4917397f87f82da76e93fcc53b7
santhosh <santhoram2002@gmail.com>
2023-01-28 01:01:57 +0530

    ns2_program_added

---
+++ ns2/distvector.tcl
@@ -0,0 +1,70 @@
+set ns [new Simulator]
+#Use distance vector routing
+$ns rtproto DV
+#Open the nam trace file
+set nf [open out.nam w]
+$ns namtrace-all $nf
+# Open tracefile
+set nt [open trace.tr w]
+$ns trace-all $nt
+#Define 'finish' procedure
+proc finish {} {
+global ns nf
+$ns flush-trace
+#Close the trace file
+close $nf
+#Execute nam on the trace file
+exec nam -a out.nam &
+exit 0
+}
+# Create 8 nodes
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
+set n7 [$ns node]
+set n8 [$ns node]
+# Specify link characterestics
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+$ns duplex-link $n2 $n3 1Mb 10ms DropTail
+$ns duplex-link $n3 $n4 1Mb 10ms DropTail
+$ns duplex-link $n4 $n5 1Mb 10ms DropTail
+$ns duplex-link $n5 $n6 1Mb 10ms DropTail
+$ns duplex-link $n6 $n7 1Mb 10ms DropTail
+$ns duplex-link $n7 $n8 1Mb 10ms DropTail
+$ns duplex-link $n8 $n1 1Mb 10ms DropTail
+# specify layout as a octagon
+$ns duplex-link-op $n1 $n2 orient left-up
+$ns duplex-link-op $n2 $n3 orient up
+$ns duplex-link-op $n3 $n4 orient right-up
+$ns duplex-link-op $n4 $n5 orient right
+$ns duplex-link-op $n5 $n6 orient right-down
+$ns duplex-link-op $n6 $n7 orient down
+$ns duplex-link-op $n7 $n8 orient left-down
+$ns duplex-link-op $n8 $n1 orient left
+#Create a UDP agent and attach it to node n1
+set udp0 [new Agent/UDP]
+$ns attach-agent $n1 $udp0
+#Create a CBR traffic source and attach it to udp0
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 set packetSize_ 500
+$cbr0 set interval_ 0.005
+$cbr0 attach-agent $udp0
+#Create a Null agent (a traffic sink) and attach it to node n4
+set null0 [new Agent/Null]
+$ns attach-agent $n4 $null0
+#Connect the traffic source with the traffic sink
+$ns connect $udp0 $null0
+#Schedule events for the CBR agent and the network dynamics
+$ns at 0.0 "$n1 label Source"
+$ns at 0.0 "$n4 label Destination"
+$ns at 0.5 "$cbr0 start"
+$ns rtmodel-at 1.0 down $n3 $n4
+$ns rtmodel-at 2.0 up $n3 $n4
+$ns at 4.5 "$cbr0 stop"
+#Call the finish procedure after 5 seconds of simulation time
+$ns at 5.0 "finish"
+#Run the simulation
+$ns run
--- ns2/wired.tcl
+++ ns2/wired.tcl
@@ -20,35 +20,34 @@ $ns namtrace-all $namfile
 
 #create nodes 
 
-set n0 [$ns node]
-set n1 [$ns node]
-set n2 [$ns node]
-set n3 [$ns node]
-set n4 [$ns node]
-set n5 [$ns node]
+for {set i 0} {$i<6} {incr i} {
+	set n($i) [$ns node]
+}
+
+
+set links {{$n(0) $n(1) 5Mb 2ms DropTail} {$n(2) $n(1) 10Mb 5ms DropTail} {$n(1) $n(4) 3Mb 10ms DropTail} {$n(4) $n(3) 100Mb 2ms DropTail} {$n(4) $n(5) 4Mb 10ms DropTail}}
+foreach link $links {
+eval $ns duplex-link $link
+}
 
 
 
-$ns duplex-link $n0 $n1 5Mb 2ms DropTail
-$ns duplex-link $n2 $n1 10Mb 5ms DropTail
-$ns duplex-link $n1 $n4 3Mb 10ms DropTail
-$ns duplex-link $n4 $n3 100Mb 2ms DropTail
-$ns duplex-link $n4 $n5 4Mb 10ms DropTail
+
 
 
 #creation of agents
 #node 0 to node3
 set udp [new Agent/UDP]
 set null [new Agent/Null]
-$ns attach-agent $n0 $udp
-$ns attach-agent $n3 $null
+$ns attach-agent $n(0) $udp
+$ns attach-agent $n(3) $null
 $ns connect $udp $null
 
 #creation of another agents
 set tcp [new Agent/TCP]
 set sink [new Agent/TCPSink]
-$ns attach-agent $n2 $tcp
-$ns attach-agent $n5 $sink
+$ns attach-agent $n(2) $tcp
+$ns attach-agent $n(5) $sink
 $ns connect $tcp $sink
 
 #creation of Application CBR,FTP
